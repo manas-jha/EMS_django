77fb75e0dd3f1fdcaf7d911918d3d5a2ff942ff1
Manas Kumar <kumarmanas2002@gmail.com>
2023-10-16 22:14:22 +0530

    Employee fetch is work correctly

--- Employee_Management_System/emp_app/views.py
+++ Employee_Management_System/emp_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 from .models import Employee, Role, Department
 from datetime import datetime
 
+
 # Create your views here.
 
 def index(request):
@@ -28,6 +29,35 @@ def add_emp(request):
         return render(request, 'add_emp.html')
 
 def remove_emp(request):
+    if request.method == 'POST':
+        
+        try:
+            if 'delete_selected' in request.POST:
+                selected_emp_id = request.POST.getlist('employee_ids')
+                Employee.objects.filter(id__in = selected_emp_id).delete()
+                print('in deleted')
+                return HttpResponse("Selected Employees detail is deleted successfully..")
+            
+            if request.POST['emp-id']:
+                emp_id = request.POST['employee_id']
+                emps = Employee.objects.filter(id = emp_id)
+                print('in emp id')
+                return render(request, 'remove_emp.html', {'employee': emps})
+            
+            print('here')
+            if 'emp-role' in request.POST:
+                employees = []
+                print("heee")
+                emps_role = request.POST['emp_role']
+                
+                lst_emps = Employee.objects.filter(role = emps_role)
+                print("The id is " , lst_emps)
+            print('in last view')
+        except:
+            pass
+            # return HttpResponse("Enter the correct Input")
+
+
     return render(request, 'remove_emp.html')
 
 def filter_emp(request):
